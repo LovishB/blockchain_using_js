85d347125c1f03fac0b415d0079a69c226b58869
Lovish Badlani <42366382+LovishB@users.noreply.github.com>
2024-06-03 02:41:34 +0530

    Created express server

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const Blockchain = require('./blockchain');
+const bodyParser = require('body-parser');
+const { PORT } = require('./config');
+
+//creating express app
+const app = express();
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+//started with blockchain, genesis block added to chain
+const blockchain = new Blockchain();
+
+//GET request to fetch blockchain data
+app.get('/api/readBlockchain', (req, res) => {
+    res.json(blockchain.chain);
+});
+
+//POST request to add new block
+app.post('/api/mineBlock', (req, res) => {
+    const data =  req.body.data;
+    blockchain.addBlock({data});
+    res.json(blockchain.chain);
+});
+
+//setting server port
+app.listen(PORT, () => {
+    console.log(`listening at localhost:${PORT}`);
+})
